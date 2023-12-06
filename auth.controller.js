3d378ca2432909d28c18526e8276943f8a0b68f6
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-12-06 12:13:23 +0100

    logica de registro + manejo de errores

--- auth.controller.js
+++ auth.controller.js
@@ -43,7 +43,24 @@ const Auth = {
             res.send(e.message)
         }
     },
-    register: async (req, res) => {},
+    register: async (req, res) => {
+        const { body } = req
+        try {
+            const isUser = await User.findOne({ email: body.email})
+            if(isUser) {
+                res.send('Usuario ya existe')
+            } else {
+               const salt = await bcrypt.genSalt()
+               const hashed = await bcrypt.hash(body.password, salt)
+               const user =  await User.create({ email: body.email, password: hashed, salt})
+
+               const signed = signToken(user._id)
+               res.send(signed)
+            }
+        } catch (err) {
+            res.status(500).send(err.message)
+        }
+    },
 }
 
 module.eports = { Auth, isAuthenticated}
