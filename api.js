bb9a44d62c38ab4e7b5b6315c53b6b5f2a71ba0d
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-12-06 12:23:48 +0100

    autentificando endpoints

--- api.js
+++ api.js
@@ -2,17 +2,21 @@ const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 const Animal = require('./animal.controller')
+const { Auth, isAuthenticated} = require('./auth.controller')
 const port = 3000
 
 mongoose.connect('mongodb://localhost:27017/miapp')
 
 app.use(express.json())
 
-app.get('/animals', Animal.list)
-app.post('/animals', Animal.create)
-app.put('/animals/:id', Animal.update)
-app.patch('/animals/:id', Animal.update)
-app.delete('/animals/:id', Animal.destroy)
+app.get('/animals', isAuthenticatedAnimal.list)
+app.post('/animals', isAuthenticatedAnimal.create)
+app.put('/animals/:id', isAuthenticatedAnimal.update)
+app.patch('/animals/:id', isAuthenticatedAnimal.update)
+app.delete('/animals/:id', isAuthenticatedAnimal.destroy)
+
+app.post('/login', Auth.login)
+app.post('/register', Auth.register)
 
 app.use(express.static('app'))
 
