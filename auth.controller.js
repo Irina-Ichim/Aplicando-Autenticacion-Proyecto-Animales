d3cf659448f4df0fb8a3f918d15a3f5b8f63cd5e
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-12-07 16:39:41 +0100

    repasando codigo

--- auth.controller.js
+++ auth.controller.js
@@ -1,67 +1,66 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
-const expressJwt = require('express-jwt');
+const express = require('express')
+const bcrypt = require('bcrypt')
+const expressJwt = require('express-jw')
+const jwt = require('jsonwebtoken')
+const User = require('./user.model')
 
-const jwt = require('jsonwebtoken');
-const User = require('./user.model');
+const validateJwt = expressJwt({secret: process.env.SECRET, algorithms: ['HS256']})
 
-const validateJwt = expressJwt({secret: process.env.SECRET, algorithms: ['HS256']});
-
-const signToken = _id => jwt.sign({ _id }, process.env.SECRET);
+const signToken = _id => jwt.sign({ _id }, process.env.SECRET)
 
 const findAndAssignUser = async (req, res, next) => {
     try {
-        const user = await User.findById(req.user._id);
+        const user = await User.findById(req.user._id)
         if (!user) {
-            return res.status(401).end();
+            return res.status(401).end()
         }
-        req.user = user;
+        req.user = user
         next();
     } catch (e) {
-        next(e);
+        next(e)
     }
 };
 
-const isAuthenticated = express.Router().use(validateJwt, findAndAssignUser);
+const isAuthenticated = express.Router().use(validateJwt, findAndAssignUser)
 
 const Auth = {
     login: async (req, res) => {
-        const { body } = req;
+        const { body } = req
         try {
-            const user = await User.findOne({ email: body.email });
+            const user = await User.findOne({ email: body.email })
             if (!user) {
-                res.status(401).send('Usuario y/o contraseña invalida');
+                res.status(401).send('Usuario y/o contraseña invalida')
             } else {
-                const isMatch = await bcrypt.compare(body.password, user.password);
+                const isMatch = await bcrypt.compare(body.password, user.password)
                 if (isMatch) {
-                    const signed = signToken(user._id);
-                    res.status(200).send(signed);
+                    const signed = signToken(user._id)
+                    res.status(200).send(signed)
                 } else {
-                    res.status(401).send('Usuario y/o contraseña invalida');
+                    res.status(401).send('Usuario y/o contraseña invalida')
                 }
             }
-        } catch (e) {
-            res.send(e.message);
+        } catch(e) {
+            res.send(e.message)
         }
     },
     register: async (req, res) => {
-        const { body } = req;
+        const { body } = req
         try {
-            const isUser = await User.findOne({ email: body.email });
+            const isUser = await User.findOne({ email: body.email })
             if (isUser) {
-                res.send('Usuario ya existe');
+                res.send('Usuario ya existe')
             } else {
-                const salt = await bcrypt.genSalt();
-                const hashed = await bcrypt.hash(body.password, salt);
-                const user = await User.create({ email: body.email, password: hashed, salt });
+                const salt = await bcrypt.genSalt()
+                const hashed = await bcrypt.hash(body.password, salt)
+                const user = await User.create({ email: body.email, password: hashed, salt })
 
-                const signed = signToken(user._id);
-                res.send(signed);
+                const signed = signToken(user._id)
+                res.send(signed)
             }
         } catch (err) {
-            res.status(500).send(err.message);
+            res.status(500).send(err.message)
         }
     },
 };
 
-module.exports = { Auth, isAuthenticated };
+module.exports = { Auth, isAuthenticated }
