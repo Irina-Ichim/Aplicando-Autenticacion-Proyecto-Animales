96a4c2d28eefcc795b4c32bfba4dcb70a79718f6
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-12-07 17:23:43 +0100

    actualizando express

--- api.js
+++ api.js
@@ -1,35 +1,35 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const app = express()
-const Animal = require('./animal.controller')
-const { Auth, isAuthenticated } = require('./auth.controller')
-const port = 3000
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const Animal = require('./animal.controller');
+const { Auth, isAuthenticated } = require('./auth.controller');
+const port = 3000;
 
+mongoose.connect('mongodb+srv://onlyirina7:LRFud1otsRVcARNx@cluster0.ijh99hh.mongodb.net/');
 
-mongoose.connect('mongodb+srv://onlyirina7:LRFud1otsRVcARNx@cluster0.ijh99hh.mongodb.net/')
+app.use(express.json());
 
-app.use(express.json())
+app.get('/animals', isAuthenticated, Animal.list);
+app.post('/animals', isAuthenticated, Animal.create);
+app.put('/animals/:id', isAuthenticated, Animal.update);
+app.patch('/animals/:id', isAuthenticated, Animal.update);
+app.delete('/animals/:id', isAuthenticated, Animal.destroy);
 
-app.get('/animals', isAuthenticated, Animal.list)
-app.post('/animals', isAuthenticated, Animal.create)
-app.put('/animals/:id', isAuthenticated, Animal.update)
-app.patch('/animals/:id', isAuthenticated, Animal.update)
-app.delete('/animals/:id', isAuthenticated, Animal.destroy)
+app.post('/login', Auth.login);
+app.post('/register', Auth.register);
 
-app.post('/login', Auth.login)
-app.post('/register', Auth.register)
-
-app.use(express.static('app'))
+app.use(express.static('app'));
 
 app.get('/', (req, res) => {
-    res.sendFile(`${__dirname}/index.html`)
+    res.sendFile(`${__dirname}/index.html`);
 });
 
 app.get('*', (req, res) => {
-    res.status(404).send('Esta página no existe :(')
+    res.status(404).send('Esta página no existe :(');
 });
 
 app.listen(port, () => {
-    console.log('Arrancando la aplicación!')
+    console.log('Arrancando la aplicación!');
 });
+
 
