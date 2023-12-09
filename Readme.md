92f5df66a2887c6519039a90c29c45ad7a3186ff
Gilda Irina Ichim <onlyirina7@gmail.com>
2023-12-09 14:41:24 +0100

    creando readme.md

---
+++ Readme.md
@@ -0,0 +1,85 @@
+# Aplicando-Autenticacion-Proyecto-Animales 🦁🐘
+
+## Descripción
+
+Aplicando-Autenticacion-Proyecto-Animales es una aplicación web simple para gestionar una lista de animales. Permite a los usuarios registrar y autenticarse, así como agregar, actualizar y eliminar animales de su lista.
+
+## Requisitos Previos
+
+Asegúrate de tener Node.js y MongoDB instalados en tu máquina antes de comenzar.
+
+## Instalación
+
+1. Clona este repositorio:
+
+   ```bash
+   git clone https://github.com/Irina-Ichim/Aplicando-Autenticacion-Proyecto-Animales.git
+   ```
+
+2. Navega al directorio del proyecto:
+
+   ```bash
+   cd Aplicando-Autenticacion-Proyecto-Animales
+   ```
+
+3. Instala las dependencias:
+
+   ```bash
+   npm install
+   ```
+
+4. Crea un archivo `.env` y configura las variables necesarias.
+
+## Uso
+
+1. Ejecuta la aplicación:
+
+   ```bash
+   npm start
+   ```
+
+2. Abre tu navegador y visita [http://localhost:3000](http://localhost:3000).
+
+## Funcionalidades
+
+- Registro de usuarios 📝
+- Inicio de sesión 🔐
+- Gestión de animales (crear, actualizar, eliminar) 🐾
+
+## Estructura del Proyecto
+
+```
+/
+|-- app/
+|   |-- main.js
+|   |-- ...
+|-- models/
+|   |-- user.model.js
+|   |-- animal.model.js
+|   |-- ...
+|-- controllers/
+|   |-- auth.controller.js
+|   |-- animal.controller.js
+|   |-- ...
+|-- index.js
+|-- ...
+```
+
+## Tecnologías Utilizadas
+
+- Node.js 🚀
+- Express 🌐
+- MongoDB 🍃
+- Mongoose 🏠
+- HTML, CSS, JavaScript (para el frontend) 🖥️
+
+## Contribuir
+
+¡Bienvenido/a a contribuir! Si deseas contribuir a AnimalApp, sigue estos pasos:
+
+1. Haz un fork del proyecto.
+2. Crea una rama para tu contribución: `git checkout -b mi-contribucion`.
+3. Realiza tus cambios y haz un commit: `git commit -m "Mi contribución"`.
+4. Envía tu contribución: `git push origin mi-contribucion`.
+5. Abre una solicitud de extracción (pull request).
+
--- app/main.js
+++ app/main.js
@@ -1,5 +1,5 @@
 const loadInitialTemplate = () => {
-    const template = `
+  const template = `
         <h1>Animales</h1>
         <form id="animal-form">
             <div>
@@ -14,74 +14,71 @@ const loadInitialTemplate = () => {
         </form>
         <ul id="animal-list"></ul>
     `;
-    const body = document.getElementsByTagName('body')[0];
-    body.innerHTML = template;
+  const body = document.getElementsByTagName("body")[0];
+  body.innerHTML = template;
 };
 
 const getAnimals = async () => {
-    const response = await fetch('/animals', {
-        headers: {
-            Authorization: localStorage.getItem('jwt')
-        }
-    });
+  const response = await fetch("/animals", {
+    headers: {
+      Authorization: localStorage.getItem("jwt"),
+    },
+  });
 
-   
-    const animals = await response.json();
-        const template = animal => `
+  const animals = await response.json();
+  const template = (animal) => `
             <li>
                 ${animal.name} ${animal.type} <button data-id="${animal._id}">Eliminar</button>
             </li>
         `;
 
-    const animalList = document.getElementById('animal-list');
-    animalList.innerHTML = animals.map((animal) => template(animal)).join('');
-        animals.forEach(animal => {
-            const animalNode = document.querySelector(`[data-id="${animal._id}"]`);
-            animalNode.onclick = async (e) => {
-                await fetch(`/animals/${animal._id}`, {
-                    method: 'DELETE',
-                    headers: {
-                        Authorization: localStorage.getItem('jwt')
-                    }
-                });
-                animalNode.parentNode.remove();
-                alert('Eliminado con éxito');
-            };
-        });
-    } 
-
+  const animalList = document.getElementById("animal-list");
+  animalList.innerHTML = animals.map((animal) => template(animal)).join("");
+  animals.forEach((animal) => {
+    const animalNode = document.querySelector(`[data-id="${animal._id}"]`);
+    animalNode.onclick = async (e) => {
+      await fetch(`/animals/${animal._id}`, {
+        method: "DELETE",
+        headers: {
+          Authorization: localStorage.getItem("jwt"),
+        },
+      });
+      animalNode.parentNode.remove();
+      alert("Eliminado con éxito");
+    };
+  });
+};
 
 const addFormListener = () => {
-    const animalForm = document.getElementById('animal-form');
-    animalForm.onsubmit = async (e) => {
-        e.preventDefault();
-        const formData = new FormData(animalForm);
-        const data = Object.fromEntries(formData.entries());
-        
-        await fetch('/animals', {
-                method: 'POST',
-                body: JSON.stringify(data),
-                headers: {
-                    'Content-Type': 'application/json',
-                    Authorization: localStorage.getItem('jwt')
-                },
-            });
-            animalForm.reset();
-            getAnimals();
-        }
-    };
+  const animalForm = document.getElementById("animal-form");
+  animalForm.onsubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(animalForm);
+    const data = Object.fromEntries(formData.entries());
 
+    await fetch("/animals", {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: localStorage.getItem("jwt"),
+      },
+    });
+    animalForm.reset();
+    getAnimals();
+  };
+};
 
-const checkLogin = () => localStorage.getItem('jwt');
+const checkLogin = () => localStorage.getItem("jwt");
 
 const animalsPage = () => {
-    loadInitialTemplate();
-    addFormListener();
-    getAnimals();
+  loadInitialTemplate();
+  addFormListener();
+  getAnimals();
 };
 
 const loadRegisterTemplate = () => {
-    const template = `
+  const template = `
         <h1>Registro</h1>
         <form id="register-form">
             <div>
@@ -97,34 +94,33 @@ const loadRegisterTemplate = () => {
         <a href="#" id="login">Iniciar sesión</a>
         <div id="error"></div>
     `;
-    const body = document.getElementsByTagName('body')[0];
-    body.innerHTML = template;
+  const body = document.getElementsByTagName("body")[0];
+  body.innerHTML = template;
 };
 
-
 const gotoLoginListener = () => {
-    const gotoLogin = document.getElementById('login');
-    gotoLogin.onclick = (e) => {
-        e.preventDefault();
-        loginPage();
-    };
+  const gotoLogin = document.getElementById("login");
+  gotoLogin.onclick = (e) => {
+    e.preventDefault();
+    loginPage();
+  };
 };
 
 const registerPage = () => {
-    console.log('Página de registro');
-    loadRegisterTemplate();
-    addRegisterListener();
-    gotoLoginListener();
+  console.log("Página de registro");
+  loadRegisterTemplate();
+  addRegisterListener();
+  gotoLoginListener();
 };
 
 const loginPage = () => {
-    loadLoginTemplate();
-    addLoginListener();
-    gotoRegisterListener();
+  loadLoginTemplate();
+  addLoginListener();
+  gotoRegisterListener();
 };
 
 const loadLoginTemplate = () => {
-    const template = `
+  const template = `
     <h1>Login</h1>
     <form id="login-form">
         <div>
@@ -140,52 +136,51 @@ const loadLoginTemplate = () => {
     <a href="#" id="register">Registrarse</a>
     <div id="error"></div>
     `;
-    const body = document.getElementsByTagName('body')[0];
-    body.innerHTML = template;
+  const body = document.getElementsByTagName("body")[0];
+  body.innerHTML = template;
 };
 
 const gotoRegisterListener = () => {
-    const gotoRegister = document.getElementById('register');
-    gotoRegister.onclick = (e) => {
-        e.preventDefault();
-        registerPage();
-    };
+  const gotoRegister = document.getElementById("register");
+  gotoRegister.onclick = (e) => {
+    e.preventDefault();
+    registerPage();
+  };
 };
 
-const authListener = action => () => {
-    const form = document.getElementById(`${action}-form`);
-    form.onsubmit = async (e) => {
-        e.preventDefault();
-        const formData = new FormData(form);
-        const data = Object.fromEntries(formData.entries());
+const authListener = (action) => () => {
+  const form = document.getElementById(`${action}-form`);
+  form.onsubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const data = Object.fromEntries(formData.entries());
 
-        const response = await fetch(`/${action}`, {
-            method: 'POST',
-            body: JSON.stringify(data),
-            headers: {
-                    'Content-Type': 'application/json',
-                },
-        });
+    const response = await fetch(`/${action}`, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
 
-        const responseData = await response.text()
-        if (response.status >= 300) {
-            const errorNode = document.getElementById('error')
-            errorNode.innerHTML = responseData
-        } else {
-            localStorage.setItem('jwt', `Bearer ${responseData}`)
-            animalsPage()
-        }
+    const responseData = await response.text();
+    if (response.status >= 300) {
+      const errorNode = document.getElementById("error");
+      errorNode.innerHTML = responseData;
+    } else {
+      localStorage.setItem("jwt", `Bearer ${responseData}`);
+      animalsPage();
     }
-}
-const addLoginListener = authListener('login')
-const addRegisterListener = authListener('register')
+  };
+};
+const addLoginListener = authListener("login");
+const addRegisterListener = authListener("register");
 
 window.onload = () => {
-    const isLoggedIn = checkLogin();
-    if (isLoggedIn) {
-        animalsPage();
-    } else {
-        loginPage();
-    }
-}
-
+  const isLoggedIn = checkLogin();
+  if (isLoggedIn) {
+    animalsPage();
+  } else {
+    loginPage();
+  }
+};
